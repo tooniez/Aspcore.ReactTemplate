c4d6422b977688012aeedde05dbb05405f0fd75f
tooniez <tooni22@proton.me>
2024-08-21 14:36:12 +0930

    feat: add db context data

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Aspnet.React.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace Aspnet.React.Data;
+
+public class ApplicationDbContext: IdentityDbContext<ApplicationUser, IdentityRole<int>, int> 
+{
+    // public DbSet<Example> Examples { get; set; } = null!;
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+        
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
