ed9665a49b5543b7cccef175cfa81ee598aec457
tooniez <tooni22@proton.me>
2024-08-21 14:34:46 +0930

    feat: add profile mappings

---
+++ Mapping/AppMappingProfile.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+
+namespace Aspnet.React.Mapping;
+
+public class AppMappingProfile : Profile
+{
+    public AppMappingProfile()
+    {
+        // CreateMap<Example, ExampleViewModel>();
+    }
+}
